b82053c402fa38a03cd72372d1f5ca9fb2009e43
jaewon <pjs941214@naver.com>
2020-04-11 14:40:04 +0900

    Frontend: Header Component

--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -11,7 +11,8 @@ export default createGlobalStyle`
         background-color:${(props) => props.theme.bgColor};
         color:${(props) => props.theme.blackColor};
         font-size:14px;
-        font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+        font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        padding-top: 140px;
     }
     a {
         color:${(props) => props.theme.blueColor};
